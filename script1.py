8b27191cabd5e0cfd5b7104d35590bdf183f4084
Mahaldeep <122818626+Mahaldeep@users.noreply.github.com>
2023-01-30 13:41:56 -0500

    Revert "Create script1.py"
    This reverts commit 0744f004d6fbf56d5f7218fe64edc6e1b7650596.

--- script1.py
+++
@@ -1,7 +0,0 @@
-#Todo- Create a dictionary that contains a full name, student ID,
-#favourite 3 pizza toppings and 2 favourite movies
-def main():
-    return
-
-if __name__=='__main__':
-    main()
